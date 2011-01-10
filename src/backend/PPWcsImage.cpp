569dc139c66fb6b1290e06a654847062caaadbd3
Amit Kapadia <devnull@localhost>
2011-01-10 17:46:23 -0600

    Displaying correct conversion to J2000 from other coordinate systems.

--- src/backend/PPWcsImage.cpp
+++ src/backend/PPWcsImage.cpp
@@ -50,6 +50,10 @@ double* PPWcsImage::pix2sky(QPointF pos)
 
 	pix2wcs(wcs, xf, yf, &world[0], &world[1]);
 	
+	// Check if coordinates are galatic
+	if (wcs->syswcs != WCS_J2000)
+		wcscon(wcs->syswcs, WCS_J2000, wcs->equinox, wcs->eqout, &world[0], &world[1], wcs->epoch);
+	
 	// Perhaps the transformationStatus needs to be checked ...
 	return world;
 }
